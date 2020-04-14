02dc201d90dd2e355321e9a154069b17a798b65e
jinho <jinho021712@gmail.com>
2020-04-14 13:16:27 +0900

    add - pytz library (time set Asia/Seoul & change template (li tag and scroll bar)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-import time
+from datetime import datetime
+from pytz import timezone
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 from flask_pymongo import PyMongo
@@ -17,8 +18,10 @@ def index():
 
 @socketio.on('message')
 def handleMessage(msg):
-    now = time.localtime()
-    get_time = "%04d/%02d/%02d %02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min)
+    now = datetime.now(timezone('Asia/Seoul'))
+    fmt = "%Y/%m/%d %H:%M"
+    get_time = now.strftime(fmt)
+    #get_time = "%04d/%02d/%02d %02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min)
     name, text = msg.split(" ")
     send_msg = f"{name} {text} [{get_time}]"
     db_data = {'name':name, 'text':text, 'get_time':get_time}
